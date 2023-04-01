154c0f453c87266d3f9c3c2fc42eb3ec8f9559d6
Project Faust <pentecost.cody.lane@gmail.com>
2023-04-01 14:40:31 -0400

    created init function; added require for generateMarkdown file

--- index.js
+++ index.js
@@ -6,19 +6,11 @@ const inquirer = require("inquirer");
 // allows interaction with file system
 const fs = require("fs");
 
-//
-const generateMarkdown = require("generateMarkdown");
+// imports generateMarkdown js
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {type: 'input',
-    message: 'What is the title of your project?',
-    name: 'title',
-    },
-    {type: 'input',
-    message: 'With which of the following licenses would you like to cover your project?',
-    name: 'license',
-    },
     {type: 'input',
     message: 'What is your GitHub usermame?',
     name: 'github',
@@ -26,14 +18,34 @@ const questions = [
     {type: 'input',
     message: 'What is your email address?',
     name: 'email',
-    }
+    },
+    {type: 'input',
+    message: 'What is the title of your project?',
+    name: 'title',
+    },
+    {type: 'list',
+    message: 'With which of the following licenses would you like to cover your project? (Use arrow keys to select).',
+    name: 'license',
+    choices: ['MIT', ]
+    default: 'MIT'
+    },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data)
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            const fileName = "README.md";
+            const data = generateMarkdown(answers);
+            writeToFile(fileName, data)
+        })
+};
 
 // Function call to initialize app
 init();
